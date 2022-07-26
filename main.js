f4788fe9f3732c0701b646dde604c7124f7cfec3
David Jacka <ahoy.dave@gmail.com>
2022-07-26 17:21:01 +0100

    Add cli args

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 const { addCommitToDate } = require('./git-util');
 const { stringToDates } = require ('./date-encode');
 
-const path = '/home/ahoydave/projects/github-secret-message/test2'
-const startDate = '2014-01-12T12:00:00';
+if (process.argv.length != 6) {
+    console.log("Wrong number of args. Usage is\nnode main.js <path> <startDate> <message> <numCommitsPerDate>\n");
+    process.exit(1);
+}
 
-const dates = stringToDates(new Date(startDate), 'HI THERE');
+const [ , , path, startDateStr, message, numCommitsPerDateStr] = process.argv;
+
+const numCommitsPerDate = Number.parseInt(numCommitsPerDateStr);
+
+if (startDateStr.match(/^\d{4}-\d{2}-\d{2}$/) == null) {
+    console.log('Date format wrong. E.g. 2014-01-12\n');
+    process.exit(1);
+}
+const startDate = new Date(startDateStr + 'T12:00:00');
+
+const dates = stringToDates(startDate, message);
+
+console.log('Adding commits for the following dates');
 dates.forEach(x => console.log(x));
 
-async function datesToCommits(path, dates) {
+async function makeCommitsOnDates(path, dates) {
     for (const date of dates) {
-        await addCommitToDate(path, date, '1', '1');
+        for (let i=0; i<numCommitsPerDate; i++) {
+            await addCommitToDate(path, date, i, i);
+        }
     }
 }
 
-datesToCommits(path, dates).then(() => {
-    console.log('Done!');
+makeCommitsOnDates(path, dates).then(() => {
+    console.log('Commits done! Now just push that repo to github and hope for the best!');
 });
